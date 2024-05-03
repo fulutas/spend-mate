7bd58263d2cf5e55b1e7d7c318d7e7b2d315752f
Furkan Ulutaş <ulutasfurkan@gmail.com>
2024-05-03 16:10:49 +0300

    shadcn component created. navbar completed.

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,15 +1,26 @@
-import { PiggyBank } from 'lucide-react'
+import { TrainFront } from 'lucide-react'
 import React from 'react'
 
 function Logo() {
-    return (
-        <a href='/' className='flex items-center gap-2'>
-            <PiggyBank className='stroke h-11 w-11 stroke-amber-500 stroke-[1.5]' />
-            <p className='bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent'>
-                SpendMate
-            </p>
-        </a>
-    )
+	return (
+		<a href='/' className='flex items-center gap-2'>
+			<TrainFront className='stroke h-11 w-11 stroke-amber-500 stroke-[1.5]' />
+			<p className='bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent'>
+				SpendMate
+			</p>
+		</a>
+	)
 }
 
-export default Logo 
+export function LogoMobile() {
+	return (
+		<a href='/' className='flex items-center gap-2'>
+			<p className='bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent'>
+				SpendMate
+			</p>
+		</a>
+	)
+}
+
+
+export default Logo  
