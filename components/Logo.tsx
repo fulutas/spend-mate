cc818b3ecaff18710eed604eb7ea6ec987894def
Furkan Ulutaş <ulutasfurkan@gmail.com>
2024-05-03 18:49:12 +0300

    dashboard development

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 function Logo() {
 	return (
 		<a href='/' className='flex items-center gap-2'>
-			<TrainFront className='stroke h-11 w-11 stroke-amber-500 stroke-[1.5]' />
-			<p className='bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent'>
+			<TrainFront className='stroke h-11 w-11 stroke-blue-400 stroke-[1.5]' />
+			<p className='bg-gradient-to-r from-blue-400 to-blue-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent'>
 				SpendMate
 			</p>
 		</a>
@@ -15,7 +15,7 @@ function Logo() {
 export function LogoMobile() {
 	return (
 		<a href='/' className='flex items-center gap-2'>
-			<p className='bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent'>
+			<p className='bg-gradient-to-r from-blue-400 to-blue-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent'>
 				SpendMate
 			</p>
 		</a>
